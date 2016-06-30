8c9dedb377106d54e37a1a5b2e88ce38fc9ebfa7
Nguyen Viet Anh <vietanh.nguyen.it@gmail.com>
2016-06-30 15:40:57 -0700

    disable yarn

---
+++ bin/bootstrap.hdfs.data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME_NODE_IP=`ping -q -c 1 $NAME_NODE_HOST | grep PING | sed -e "s/).*//" | sed -e "s/.*(//"`
+# NODE_MANAGER_IP=`ping -q -c 1 $NODE_MANAGER_HOST | grep PING | sed -e "s/).*//" | sed -e "s/.*(//"`
+
+sed -i "s/@IP@/$NAME_NODE_IP/g" $HADOOP_CONF_DIR/core-site.xml
+# sed -i "s/@NODE_MANAGER_IP@/$NODE_MANAGER_IP/g" $HADOOP_CONF_DIR/yarn-site.xml
+
+echo "$NAME_NODE_IP $NAME_NODE_HOST" >> /etc/hosts
+
+/usr/sbin/sshd && start-dfs.sh
--- bin/bootstrap.hdfs.name.sh
+++ bin/bootstrap.hdfs.name.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 IP=`ip addr show eth0 | grep 'inet ' | cut -d/ -f1 | awk '{print $2}'`
-sed -i "s/@IP@/$IP/g" $HADOOP_CONF_DIR/core-site.xml
+# NODE_MANAGER_IP=`ping -q -c 1 $NODE_MANAGER_HOST | grep PING | sed -e "s/).*//" | sed -e "s/.*(//"`
 
-echo "$IP \t$(hostname)" >> /etc/hosts
+sed -i "s/@IP@/$IP/g" $HADOOP_CONF_DIR/core-site.xml
+# sed -i "s/@NODE_MANAGER_IP@/$NODE_MANAGER_IP/g" $HADOOP_CONF_DIR/yarn-site.xml
+
+# echo "$IP \t$(hostname)" >> /etc/hosts
 
 hdfs namenode -format \
     && /usr/sbin/sshd \
-    && start-yarn.sh \
     && start-dfs.sh && hdfs dfs -mkdir -p /hbase && hdfs dfs -chown root:root /hbase
