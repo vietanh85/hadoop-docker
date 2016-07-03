b00ef2cffc976f6fa2fb1b05e2e22ef5bb067c77
Nguyen Viet Anh <vietanh.nguyen.it@gmail.com>
2016-07-03 09:40:50 -0700

    minor change

--- bin/bootstrap.pseudo.sh
+++ bin/bootstrap.pseudo.sh
@@ -3,7 +3,7 @@
 IP=`ip addr show eth0 | grep 'inet ' | cut -d/ -f1 | awk '{print $2}'`
 
 sed -i "s/@IP@/$IP/g" $HADOOP_CONF_DIR/core-site.xml
-sed -i "s/@NODE_MANAGER_IP@/$IP/g" $HADOOP_CONF_DIR/yarn-site.xml
+sed -i "s/@YARN_IP@/$IP/g" $HADOOP_CONF_DIR/yarn-site.xml
 
 hdfs namenode -format \
     && /usr/sbin/sshd \
